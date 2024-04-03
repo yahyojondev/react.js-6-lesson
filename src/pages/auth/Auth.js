2f617fb1d6950857c1630a58933f1e2529ac387c
yahyojondev <nabijonovdeveloper@gmail.com>
2024-04-03 12:29:16 +0500

    hello

--- src/pages/auth/Auth.js
+++ src/pages/auth/Auth.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Navigate, Outlet} from "react-router-dom"
 
 function Auth() {
-    let user = "john"
+    let user =(null)
     return user ? <Outlet/> : <Navigate replace to = {"/login"}/>
  
 }
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -13,9 +13,9 @@ function Login() {
          </div>
          <div className="login__form">
             <h5 className="input__title">Email Addres</h5>
-            <input value="nabijonovdev@gmail.com" type="text" placeholder='Enter your email' />
+            <input  type="text" placeholder='Enter your email' />
             <h5 className="input__title">Password</h5>
-            <input value="saber2303" type="text" placeholder='Enter your password' />
+            <input  type="text" placeholder='Enter your password' />
             <ul className="login__list">
               <li className="login__item"> <span className="login__span"></span><a href="#" className="login__link">Remember Me</a></li>
               <li className="login__item"><a href="#" className="login__link">Forgot Password</a></li>
